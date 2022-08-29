41766fef58324933d28c72d2ecd6382623e670a0
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-08-29 21:00:46 +0200

    app.js
    app.js

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Business Site
 
-Replace this readme with your own information about your project. 
+Freelance wed-developer website. 
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
+I wanted to hide the answer, I used the toggle function to adds and remove the class "active" on the section you click on.
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I used grid to have responsive page. 
 
 ## View it live
 Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+const questions = document.getElementsByClassName('accordion');
+let i;
+for(i = 0; i < questions.length; i++) {
+  questions[i].addEventListener('click', function ()  {
+    this.classList.toggle("active");
+    const answer= this.nextElementSibling;
+    if (answer.style.display == "block"){
+      answer.style.display="none";
+    }else{
+      answer.style.display="block"
+    }
+  })
+}
